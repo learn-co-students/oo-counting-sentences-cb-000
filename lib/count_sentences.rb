e6f0429abba1c24bd94f22bfed0db57d396b01fb
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2016-12-12 05:36:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/(?<=[.?!;])\s+(?=\p{Lu})/i)
+    i = 0
+    sentences.each do |sentence|
+      i += 1
+    end
+    return i
   end
 end
