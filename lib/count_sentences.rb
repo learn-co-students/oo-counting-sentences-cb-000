3c0ee1ff2be35a1597ca591e4b30392976e4c8ec
Andrew Schubert <andrew.schubert1986@gmail.com>
2017-02-07 19:08:03 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,6 @@ class String
   end
 
   def count_sentences
-    binding.pry
-    self.split(/\.|\?|\!/).delete_if {|word| word.length < 2}.length
+    self.split(/\.|\?|\!/).delete_if {|word| word.size < 2}.size
   end
 end
