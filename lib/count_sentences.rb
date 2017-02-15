2ab07fc305ff3ba87d88faa049d64f3a50e5b5fb
Tyler Bristol <tahbristol@gmail.com>
2017-02-15 03:32:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    self.end_with?("?") ?  true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+      array = []
+      array = self.scan(/[^\.!?]+[\.!?]/)
+      count = array.length
 
+      count
   end
 end
