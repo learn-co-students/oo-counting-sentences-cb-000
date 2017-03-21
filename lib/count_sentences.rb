05f40a80631a0055a000a0f7340b729e5920a8de
Brian Johnson <overclocker303@gmail.com>
2017-03-21 17:08:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    my_array = self.split /[.!?]/
+    my_array.delete('')
+    my_array.count
   end
 end
