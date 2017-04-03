21e8d8ddf67bdf0504791cab15703625f5d60c97
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-04-03 09:27:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    sentences_raw = self.split(%r{(\.|\!|\?)})
+    punctuation_marks = [".", "!", "?", " ", ""]
+    punctuation_marks.each do |punctuation_mark|
+      sentences_raw.delete_if {|sentence| sentence == punctuation_mark}
+    end
+    sentences_raw.count
+    #binding.pry
   end
 end
+
+
+#text = "one. two. three?"
+#text.count_sentences
