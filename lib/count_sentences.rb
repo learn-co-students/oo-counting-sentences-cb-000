4f611510133230d28f35c1444653ed419f495c26
Kevin Tran <kevindutran@gmail.com>
2017-05-14 00:37:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    parsed = self.split(/[.?!]/);
+    if parsed.include?("")
+      parsed.delete("")
+      return parsed.count
+    else
+      return parsed.count
+    end
   end
 end
