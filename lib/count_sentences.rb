024831cd9af5b135c0e6223df29e06b187001b0d
chris u <cu364@nyu.edu>
2017-10-12 19:12:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    number_of_sentc = self.split(%r{[!?.]\s})
+    number_of_sentc.size
   end
+
 end
