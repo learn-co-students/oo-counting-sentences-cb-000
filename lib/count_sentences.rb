aaf354a224ab471ddb973b69ae6ac22204169c2b
Mark Cruz <markchriscruz@yahoo.com>
2018-03-11 21:30:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    collector = self.split(/\?+|\.+|\!+/)
+    return collector.count
   end
 end
