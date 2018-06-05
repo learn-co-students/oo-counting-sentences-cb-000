b46bd3d01f0940ac2a66435839a881a5b3c4ba06
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-05 21:33:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    a = self.split(/[!.?]+/)
+    a.delete_if {|b| b == " "}
+    a.count
   end
 end
+
+
+#    {|b| if b.length == 0 then delete!}
+#    a.delete_if{|b| b.length == 0}
+
+
+#    a.each do |b|
+#      if b.length == 0
+#        a.delete!(b)
+#      end
+#    end
+
+#This, well, is a sentence. | This is too!! | And so is this, I think? | Woo..."
