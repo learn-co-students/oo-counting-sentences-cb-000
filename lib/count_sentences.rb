662ddae08fbe4b9d704a4aeb3892478ba1b24d9a
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-08-11 20:37:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    sentences = self.split(/[.!?]/)
+    sentences.each do |sentence|
+      if sentence != ""
+        count += 1
+      end
+    end 
+    binding.pry
+    count
   end
 end
