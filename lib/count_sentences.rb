d7adc24f5afaf0fc1ebb664f4ac997cd600cf90b
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-18 03:05:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[-1] == "."  
   end
 
   def question?
-
+    return self[-1] == "?" 
   end
 
   def exclamation?
-
+    return self[-1] == "!" 
   end
 
   def count_sentences
-
+    count = 0
+    array = self.split(/[?!.]/)
+    array.each do |sentence| 
+    if !(sentence == nil) && !(sentence == "")
+      count += 1 
+      end
+    end
+    return count
   end
 end
