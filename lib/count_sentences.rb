88abee72d5373e7f4da3c608f9888c9ef6b83640
Natasha Waller <natashawaller@Natashas-MacBook-Pro.local>
2019-01-08 13:58:01 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    array_of_sentences = self.split (/[.?!]+/) 
+    array_of_sentences.size  
   end
 end
