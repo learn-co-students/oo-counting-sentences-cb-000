4484bb6681ad4992cdcf647bc3f89e7d6e5f5732
Steve Cordova <cordovas@bc.edu>
2020-07-31 17:06:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
 
+    self.split(/[.!?]\s/).count
+
+    # "This is a string! It has three sentences. Right?".count_sentences
+    # # => 3
   end
 end
